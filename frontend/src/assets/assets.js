b9b6c0842661a6f725b303afcf6b3a860bce1cad
aaryanraj3 <aaryanraj7273@gmail.com>
2025-04-22 17:00:47 +0530

    podcast

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -14,6 +14,7 @@ import echobeats_logo from "./echobeats_logo.ico";
 import echobeats_hq_logo from "./echobeatshqlogo.jpg";
 import mute_icon from "./mute.png";
 
+import podcast_icon from "./podcast.png";
 
 
 export const assets = {
